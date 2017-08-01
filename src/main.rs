47b97d874834aa0dcc4fa8d8b3a2e141cb34f9da
Gray Olson <gray@grayolson.com>
2017-08-01 16:25:40 -0700

    add wait flag

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,54 @@ use std::cmp;
 use std::time::Duration;
 use std::thread;
 use std::env;
+use std::io;
 
 fn main() {
 
     let mut args = env::args();
     args.next();
+
     let n: u16 = match args.next() {
-        Some(arg)  => arg.parse()
+        Some(arg)   => arg.parse()
             .expect("First argument should be a positive integer number"),
         None        => 5u16,
     };
-    toh_solve(n);
-}
 
-fn toh_solve(num_slabs: u16) {
+    let wait: bool = match args.next() {
+        Some(arg)   => {
+            if arg == String::from("--wait") {true} else {false}
+        },
+        None        => false,
+    };
 
     let mut tower1: Vec<u16> = Vec::new();
-    for i in (1..num_slabs+1).rev() {
+    for i in (1..n+1).rev() {
         tower1.push(i);
     }
     let mut tower2: Vec<u16> = Vec::new();
     let mut tower3: Vec<u16> = Vec::new();
     let mut state = vec![&mut tower1, &mut tower2, &mut tower3];
-    render(&mut state);
-    toh(num_slabs,0,2,1,&mut state);
+    render(&mut state,wait);
+    toh(n,0,2,1,&mut state,wait);
 }
 
-fn toh(slab: u16, src: u16, dest: u16, aux: u16, state: &mut Vec<&mut Vec<u16>>) {
+fn toh(slab: u16, src: u16, dest: u16, aux: u16, state: &mut Vec<&mut Vec<u16>>, wait: bool) {
     if slab == 1 {
-        do_move(slab, src, dest, state);
+        do_move(slab, src, dest, state, wait);
         return;
     }
-    toh(slab-1, src, aux, dest, state);
-    do_move(slab, src, dest, state);
-    toh(slab-1, aux,dest, src, state);
+    toh(slab-1, src, aux, dest, state, wait);
+    do_move(slab, src, dest, state, wait);
+    toh(slab-1, aux,dest, src, state, wait);
 }
 
-fn do_move(slab: u16, src: u16, dest: u16, state: &mut Vec<&mut Vec<u16>>) {
+fn do_move(slab: u16, src: u16, dest: u16, state: &mut Vec<&mut Vec<u16>>, wait: bool) {
     state[src as usize].pop();
     state[dest as usize].push(slab);
-    render(state);
+    render(state,wait);
 }
 
-fn render(state: &mut Vec<&mut Vec<u16>>) {
+fn render(state: &mut Vec<&mut Vec<u16>>, wait: bool) {
     print!("{}[2J", 27 as char);
 
     let height = state.iter().fold(0,
@@ -73,9 +78,15 @@ fn render(state: &mut Vec<&mut Vec<u16>>) {
         println!("{}   {}   {}", slabs[0], slabs[1], slabs[2]);
     }
     let width = (height*2+1)*3 + 6;
-    let table = (0..width)
+    let barrier = (0..width)
         .fold(String::from(""), |acc, _| format!("{}{}",acc,"#"));
-    println!("{}", table);
+    println!("{}", barrier);
 
-    thread::sleep(Duration::from_millis(cmp::max(2000/height as u64, 20)));
+    if wait {
+        println!("Press Enter to execute next move");
+        let mut string_in = String::new();
+        io::stdin().read_line(&mut string_in).expect("failed to read line");
+    } else {
+        thread::sleep(Duration::from_millis(cmp::max(2000/height as u64, 30)));
+    }
 }
